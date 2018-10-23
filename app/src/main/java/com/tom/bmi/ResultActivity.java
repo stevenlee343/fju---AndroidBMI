e1f5af5721a6da1f5fbdb72448dae79a8dfe5195
Steven Lee <stxx0304@gmail.com>
2018-10-23 15:02:50 +0800

    1023 3pm

--- app/src/main/java/com/tom/bmi/ResultActivity.java
+++ app/src/main/java/com/tom/bmi/ResultActivity.java
@@ -2,6 +2,7 @@ package com.tom.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        float bmi = getIntent().getFloatExtra("BMI",0);
+        TextView result = findViewById(R.id.result);
+        result.setText("Your BMI is " + bmi);
     }
 }
